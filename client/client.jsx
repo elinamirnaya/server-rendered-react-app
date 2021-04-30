6a2365792c4b9469713b8274aae43c1e3eca5947
Elina Samoilenko <elina_samoilenko@epam.com>
2021-04-30 19:09:12 +0300

    Rehydrate interactive React component

--- client/client.jsx
+++ client/client.jsx
@@ -3,4 +3,31 @@ import ReactDOM from 'react-dom';
 
 import { App } from './App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+let state = undefined;
+
+fetch("http://localhost:7777/data").then(data => data.json()).then(json => {
+    state = json;
+    console.info('Got the state', state);
+
+    render();
+})
+
+// ReactDOM.render(<App />, document.querySelector('#root'));
+
+function handleModifyAnswerVotes(answerId, increment) {
+    state.answers = state.answers.map(answer => {
+        if (answer.answerId !== answerId) {
+            return answer;
+        } else {
+            return {...answer, upvotes: answer.upvotes + increment }
+        }
+    });
+
+    render();
+}
+
+function render() {
+    ReactDOM.hydrate(<App {...state} handleModifyAnswerVotes={handleModifyAnswerVotes} />, document.querySelector('#root'));
+}
+
+// render();
--- server/index.js
+++ server/index.js
@@ -48,7 +48,11 @@ const app = new express();
 
 app.use(express.static("dist"));
 
-app.get('/', (_req, res) => {
+app.get('/data', async (_req, res) => {
+    res.json(data);
+});
+
+app.get('/', async (_req, res) => {
     const index = readFileSync('public/index.html', 'utf8');
     const component = renderToString(<App {...data} />);
 
