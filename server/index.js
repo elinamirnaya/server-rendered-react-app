20372374ada86517e58213dcb1a713b105a35c00
Elina Samoilenko <elina_samoilenko@epam.com>
2021-04-30 09:08:46 +0300

    Scaffold an environment for server rendering

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { readFileSync } from 'fs';
+
+const app = new express();
+
+app.use(express.static("dist"));
+
+app.get('/', (_req, res) => {
+    const index = readFileSync('public/index.html', 'utf8');
+
+    res.send(index);
+});
+
+app.listen(7777);
+console.info('Server is listening on http://localhost:7777.');
