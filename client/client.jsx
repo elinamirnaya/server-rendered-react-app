6e1a8d5da16593de9f6df4bf88bb302606290722
Elina Samoilenko <elina_samoilenko@epam.com>
2021-04-30 19:29:43 +0300

    Share code between client and server

--- client/client.jsx
+++ client/client.jsx
@@ -2,32 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { App } from './App';
+import { handleModifyAnswerVotes } from '../shared/utility';
 
 let state = undefined;
 
-fetch("http://localhost:7777/data").then(data => data.json()).then(json => {
+fetch("data").then(data => data.json()).then(json => {
     state = json;
     console.info('Got the state', state);
 
     render();
 })
 
-// ReactDOM.render(<App />, document.querySelector('#root'));
+function handleVote(answerId, increment) {
+    state.answers = handleModifyAnswerVotes(state.answers, answerId, increment);
 
-function handleModifyAnswerVotes(answerId, increment) {
-    state.answers = state.answers.map(answer => {
-        if (answer.answerId !== answerId) {
-            return answer;
-        } else {
-            return {...answer, upvotes: answer.upvotes + increment }
-        }
-    });
+    fetch(`vote/${answerId}?increment=${increment}`);
 
     render();
 }
 
 function render() {
-    ReactDOM.hydrate(<App {...state} handleModifyAnswerVotes={handleModifyAnswerVotes} />, document.querySelector('#root'));
+    ReactDOM.hydrate(<App {...state} handleModifyAnswerVotes={handleVote} />, document.querySelector('#root'));
 }
-
-// render();
